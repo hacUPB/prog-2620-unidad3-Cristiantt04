5fc105c25350cb9d7fc299e3892ada1afe80a8ef
Cristian Martinez <cristian.martinezt@upb.edu.co>
2026-02-27 15:38:02 -0500

    Se adelanta las actividades del viernes 27/02

---
+++ Resultados de codigos/Operadores.md
@@ -0,0 +1,5 @@
+## 1. Operadores Aritméticos
+
+### Ejercicio 1: La cuenta del restaurante. 
+![Resultado_Primercodigo](../Imagenes/Imagen1.png)
+
---
+++ SRC/ejercicio2_relacionales.py
@@ -0,0 +1,8 @@
+### Ejercicio 2: El guardián de la montaña.
+Altura_visitante = 120
+Altura_minima = 150
+
+puede_pasar = Altura_visitante >= Altura_minima
+No_puede_pasar = Altura_visitante < Altura_minima
+
+print("El usuario puede pasar", puede_pasar)
